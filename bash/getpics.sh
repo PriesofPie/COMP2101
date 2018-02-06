7099a8b59f48fc83e88a7a270c636c056d30d277
PriesofPie <matt.pries@hotmail.com>
2018-02-06 05:35:00 -0800

    changes made to rm pics.zip (move lines)

--- bash/getpics.sh
+++ bash/getpics.sh
@@ -8,6 +8,7 @@ wget -nv http://zonzop.net/pics.zip
 
 #Assumes we retrjeved a zip file, hardcoded ******
 unzip -q pics.zip
+rm pics.zip
 
 #Sample output :
 # Found 95 regular files using 7.1M of storage in the ~/Pictures directory
@@ -16,4 +17,3 @@ find . -type f|wc -1
 echo -n " regular files using "
 du -sh .|awk '{print $1}'
 echo " of storage in the ~/Pictures directory"
-rm pics.zip
