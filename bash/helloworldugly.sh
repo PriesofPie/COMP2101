e45692eca6303e9ab142fe6bd2d5439e8cb68272
PriesofPie <35733686+PriesofPie@users.noreply.github.com>
2018-02-06 08:06:47 -0500

    Update helloworldugly.sh

--- bash/helloworldugly.sh
+++ bash/helloworldugly.sh
@@ -3,4 +3,4 @@
 # This script displays the string "Hello World!"
 
 #this is a silly way of creating the output text by starting with something else and stream editing it in a pipeline
-echo -n "helb wold" |sed -e "s/b/o/b" -e "s/l/ll/" -e "s/ol/orl/" |tr "h" "H"|tr "w" "W"|awk '{print $1 "\x20" $2 "\41"}'
+echo -n "helb wold" |sed -e "s/b/o/g" -e "s/l/ll/" -e "s/ol/orl/" |tr "h" "H"|tr "w" "W"|awk '{print $1 "\x20" $2 "\41"}'
